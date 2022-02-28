4816d522d4ea71349256a4d4700ea74840ba0b69
mizza-cz <mizza.cz666@gmail.com>
2022-02-28 11:39:43 +0100

    test

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$('.nav a').on('click', function() {
+
+   let href = $(this).attr('href');
+
+   $('html, body').animate({
+       scrollTop: $(href).offset().top
+   }, {
+       duration: 370,   // по умолчанию «400» 
+       easing: "linear" // по умолчанию «swing» 
+   });
+
+   return false;
+});
+
+$('.mob-menu').on('click', function () {
+    $('.nav').slideToggle();
+  });
