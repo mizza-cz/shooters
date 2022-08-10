96973db8e5fe6c4840cd26752de48db946fdfc07
mizza-cz <mizza.cz666@gmail.com>
2022-08-10 09:55:54 +0200

    add fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,25 @@
-
 var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-    return false;
+$('a[href*="#"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    500,
+  );
+  return false;
 });
 
 $('.mob-menu').on('click', function () {
-    $('.nav').slideToggle();
-  });
+  $('.nav').slideToggle();
+});
 
-let menu = document.getElementById('header')
-let section = document.getElementById('muzi')
-window.onscroll = function(){
-    if(window.pageYOffset >= section.offsetTop){
-        menu.classList.add('sticky')
-    }
-    if(window.pageYOffset < section.offsetTop){
-        menu.classList.remove('sticky')
-    }
-}
+let menu = document.getElementById('header');
+let section = document.getElementById('muzi');
+window.onscroll = function () {
+  if (window.pageYOffset >= section.offsetTop) {
+    menu.classList.add('sticky');
+  }
+  if (window.pageYOffset < section.offsetTop) {
+    menu.classList.remove('sticky');
+  }
+};
